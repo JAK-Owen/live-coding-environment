02153d669707abdbf06ef79a6ea5144971fcebd4
JAKOwen-gh <jakowen@protonmail.com>
2021-01-02 20:30:29 +0000

    Setup code editor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,93 @@
+import React, {Component} from 'react';
+import './App.css';
+import * as Tone from 'tone';
+import ScriptTag from 'react-script-tag';
+import AceEditor from 'react-ace';
+import { Player, Buffer } from 'tone';
 
 
-function App() {
+class App extends Component {
+  constructor(props) {
+     super(props);
+     this.go = this.go.bind(this);
+
+   }
+
+play(){
+console.log('d');}
+
+go() {
+  Tone.context.latencyHint = 'fastest';
+  Tone.Transport.bpm.value = 130;
+  var editor = editor.edit('editor');
+  var seq = new Tone.Sequence(function(time, note){
+  console.log(note);
+     //subdivisions are given as subarrays
+     }, ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]);
+        Tone.Transport.start('+0.2');
+        seq.start();
+}
+
+render() {
+var vol = new Tone.Volume(-12).toMaster();
+
+    var p1 = new Player({
+     "hihat": 'https://cdn.jsdelivr.net/gh/Tonejs/Tone.js/examples/audio/505/hh.mp3'
+    }, function()
+    {
+     //console.log('loaded');
+    });
+
+    p1.get('hihat');
+    p1.autostart = true;
+
+
+
+
   return (
+
     <div className="App">
-  
-    </div>
+
+    <button className='go' onClick={this.go}>play</button>
+
+    <div className='editor' id="editor"></div>
+
+
+
+    <AceEditor
+
+                    mode="javascript"
+                    theme='monokai'
+                    fontSize={20}
+                    onChange={this.onChange}
+                    name="editor"
+                    editorProps={{
+                        $blockScrolling: true
+                    }}
+                    setOptions={{
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: true
+    }}
+
+
+
+    value={`
+    if ([0,4,8,12].indexOf(idx) >= 0)
+       p1.get('kick').start();
+
+    if ([2,6,10,14].indexOf(idx) >= 0)
+             p1.get('snare').start();
+`}
+                />
+
+
+
+
+
+</div>
   );
+}
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import * as Tone from "tone";
+
+export default function Test() {
+  const synth = new Tone.Synth().toDestination();
+
+  function playNote(note) {
+    synth.triggerAttackRelease(`${note}4`, "8n");
+  }
+  return (
+    <div className="App">
+      <div className="note-wrapper">
+        <button className="note" onClick={() => playNote("C")}>
+          C
+        </button>
+        <button className="note" onClick={() => playNote("D")}>
+          D
+        </button>
+        <button className="note" onClick={() => playNote("E")}>
+          E
+        </button>
+        <button className="note" onClick={() => playNote("G")}>
+          G
+        </button>
+        <button className="note" onClick={() => playNote("A")}>
+          A
+        </button>
+      </div>
+    </div>
+  );
+}
